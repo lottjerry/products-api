69ef32463582854cb60f52726caea0938696e467
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-09-14 01:46:02 -0500

    Fixed corsOption

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import "dotenv/config";
 import express from 'express'
+import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 
-const cors = require('cors')
 const app = express()
 const prisma = new PrismaClient()
 
-app.use(cors())
 const corsOptions ={
     origin:'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
